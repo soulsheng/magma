9cea5ac80018122376e31695959da953bda147ff
s <s>
2014-03-26 15:16:17 +0800

    generate vs2010 project , add a line "set (CMAKE_Fortran_COMPILER_FORCED true)" to pass

--- include/mangling.h
+++ include/mangling.h
@@ -1,7 +1,4 @@
-#ifndef MANGLING_H
-#define MANGLING_H
+#ifndef MAGMA_HEADER_INCLUDED
+#define MAGMA_HEADER_INCLUDED
 
-/* Empty file. See magma_mangling.h
- * When using CMake, this gets replaced by Fortran name mangling. */
-
-#endif        //  #ifndef MANGLING_H
+#endif
